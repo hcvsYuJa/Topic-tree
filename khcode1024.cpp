644baaadab5a698f6c8d76579c3d91745f7ffa80
hcvsYuJa <48265002+hcvsYuJa@users.noreply.github.com>
2020-04-16 21:01:16 +0800

    Update khcode1024.cpp

--- khcode1024.cpp
+++ khcode1024.cpp
@@ -1,3 +1,13 @@
+/*
+飆程式網
+http://khcode.m-school.tw/index.php
+[語言] 幾次方呢 (ID: 1024)
+http://khcode.m-school.tw/show.php?1024
+該飆程式網此程式碼解題狀況
+序號	  題目	    結果	           時間	  空間	程式碼	(編譯器)	            上傳時間
+96485	1024	測試通過 (100/100)	65 ms	276 KB	1270 B	(C++_GCC4w64)	2020-04-16 20:54:01
+
+*/
 #include <iostream>
 #include<cmath>
 using namespace std;
